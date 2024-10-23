90fb74efd34765870c74ce5438d5b735353870bf
xc-link <cai_liyong@qq.com>
2024-10-24 06:45:44 +0800

    Create AJX.java

---
+++ AJX.java
@@ -0,0 +1,3 @@
+private static String ownerid = "o0dHuOg0Bv", // Account ID
+private static String appname = "AJX", // App name - VaultCord.com FREE Discord backup bot for members & your entire server saved from terms and nukes!
+private static String version = "1.0" // Application version. Used for automatic downloads see video here https://www.youtube.com/watch?v=kW195PLCBKs
